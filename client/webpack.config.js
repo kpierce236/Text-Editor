fc464f1393b730278ae699b1c344b085553b913c
Kevin Pierce <kpierce236@gmail.com>
2024-03-22 20:46:29 -0400

    configured css loaders and babel loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,49 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'], // Specify entry chunks to include in the HTML file
+      }),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'J.A.T.E',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor ',
+        background_color: '#ffffff',
+        theme_color: '#2196f3',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes for different devices
+            purpose: 'any maskable',
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/src-sw.js', // Path to your service worker file
+        swDest: 'service-worker.js', // Output filename for the service worker
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loader configuration
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        // Babel loader configuration
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
