7ee0fcb0b1ca022181fbcda35f20035aac265e07
Kevin Pierce <kpierce236@gmail.com>
2024-03-22 20:03:44 -0400

    added putDb function to store data in indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.add({ content });
+    await tx.done;
+    console.log('Content added to the database successfully');
+  } catch (error) {
+    console.error('Error adding content to the database:', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
