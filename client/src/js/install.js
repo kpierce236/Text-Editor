d87a962dbf1d34388e3c3f9d43739967e3b27663
Kevin Pierce <kpierce236@gmail.com>
2024-03-22 20:10:28 -0400

    added the beforeInstall event listener

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,17 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let deferredPrompt; // To store the event for later use
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the default behavior
+    event.preventDefault();
+    // Store the event for later
+    deferredPrompt = event;
+    // Show the install button
+    butInstall.style.display = 'block';
+  });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
